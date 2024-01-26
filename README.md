b76ae61ce5bd72caae6d7662a0133773ebee1cc6
Yaroslava Shyt <yaroslavashyt@gmail.com>
2024-01-26 16:32:24 +0200

    AboutListTile, AbsorbPointer, AlertDialog, Align

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ All flutter widgets in one app
 ## Used widgets
 
 - AboutDialog - show app name, version, legalese; view licenses listed for your app
+- AboutListTile - show app name, version, legalese; view licenses listed for your app
+- AbsorbPointer - makes an element not active (cannot press the button or mark a checkbox)
+- AlertDialog - dialog with icon, title, content, paddings, buttons...
+- Align - sets the place where to put the child widget
--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,13 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
-            child: ElevatedButton(
+            child: ListView(
+          children: [
+            ElevatedButton(
                 onPressed: () {
                   showDialog(
                       context: context,
+                      // 1
                       builder: (context) => const AboutDialog(
                             applicationIcon: FlutterLogo(),
                             applicationLegalese: 'Legalese',
@@ -51,6 +54,95 @@ class _MyHomePageState extends State<MyHomePage> {
                             ],
                           ));
                 },
-                child: const Text("Show about dialog"))));
+                child: const Text("Show about dialog")),
+            // 2
+            const AboutListTile(
+              icon: Icon(Icons.info),
+              applicationIcon: FlutterLogo(),
+              applicationLegalese: 'Legalese',
+              applicationName: 'Flutter widgets',
+              applicationVersion: '1.0.0',
+              aboutBoxChildren: [Text('About list tile text')],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: <Widget>[
+                SizedBox(
+                  width: 200.0,
+                  height: 100.0,
+                  child: ElevatedButton(onPressed: () {}, child: null),
+                ),
+                SizedBox(
+                  width: 100.0,
+                  height: 200.0,
+                  // 3
+                  child: AbsorbPointer(
+                      child: ElevatedButton(
+                    onPressed: () {},
+                    child: null,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue.shade200),
+                  )),
+                ),
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    // 4
+                    builder: (context) => AlertDialog(
+                      icon: const FlutterLogo(),
+                      iconPadding: const EdgeInsets.all(20.0),
+                      iconColor: Colors.blue.shade200,
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Close'))
+                      ],
+                      actionsPadding: const EdgeInsets.all(20.0),
+                      actionsAlignment: MainAxisAlignment.center,
+                      actionsOverflowAlignment: OverflowBarAlignment.center,
+                      actionsOverflowButtonSpacing: 20.0,
+                      actionsOverflowDirection: VerticalDirection.up,
+                      alignment: Alignment.topRight,
+                      elevation: 20.0,
+                      backgroundColor: Colors.deepOrangeAccent,
+                      buttonPadding: const EdgeInsets.all(20.0),
+                      scrollable: true,
+                      semanticLabel: 'Alert',
+                      shadowColor: Colors.cyanAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      title: const Text('Alert dialog'),
+                      titlePadding: const EdgeInsets.all(20.0),
+                      titleTextStyle: const TextStyle(color: Colors.amber),
+                      content: const Text('This is the Alert Dialog'),
+                      contentPadding: const EdgeInsets.all(20.0),
+                      contentTextStyle: const TextStyle(color: Colors.brown),
+                      clipBehavior: Clip.antiAlias,
+                      insetPadding: const EdgeInsets.all(20.0),
+                    ),
+                  );
+                },
+                child: const Text('Show alert dialog')),
+            Container(
+              height: 120,
+              width: double.infinity,
+              color: Colors.amber,
+              //alignment: Alignment.topLeft,
+              // 5
+              child: const Align(
+                alignment: Alignment.bottomRight,
+                child: FlutterLogo(
+                  size: 60,
+                ),
+              ),
+            )
+          ],
+        )));
   }
 }
