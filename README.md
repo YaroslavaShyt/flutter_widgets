e9785bf553158d2208bf8e1283c4e13f4237c7c6
Yaroslava Shyt <yaroslavashyt@gmail.com>
2024-02-27 12:53:35 +0200

    - animated align

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ All flutter widgets in one app
 - AbsorbPointer - makes an element not active (cannot press the button or mark a checkbox)
 - AlertDialog - dialog with icon, title, content, paddings, buttons...
 - Align - sets the place where to put the child widget
+- AnimatedAlign  - animates changing of alignment of an item
--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool selected = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -141,6 +142,28 @@ class _MyHomePageState extends State<MyHomePage> {
                   size: 60,
                 ),
               ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selected = !selected;
+                });
+              },
+              child: Center(
+                  child: Container(
+                width: double.infinity,
+                height: 250.0,
+                color: Colors.blueGrey,
+                child: AnimatedAlign(
+                  alignment:
+                      selected ? Alignment.topRight : Alignment.bottomLeft,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.fastOutSlowIn,
+                  child: const FlutterLogo(
+                    size: 50.0,
+                  ),
+                ),
+              )),
             )
           ],
         )));
